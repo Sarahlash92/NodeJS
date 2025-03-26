616bbc1e08675769ad67f39d601a5e3ff366c928
Sarah Lash <sarah.lash92@gmail.com>
2025-03-26 18:02:04 +0100

    connecting mongoose

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import "reflect-metadata";
 
 import express, {Request, Response } from "express";
 import { addRoutes } from "./src/config/routes.config";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv"
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response ) => {
     res.send("Express application");
@@ -12,8 +14,27 @@ app.get("/", (req: Request, res: Response ) => {
 
 addRoutes(app);
 
-app.listen(port, () => {
-    console.log(`Server running at http:localhost:${port}`);
-});
+async function bootstrap() {
+
+    if (!process.env.DATABASE_URL) {
+        throw new Error("Cannot read env variables");
+    }
+    try {
+        await mongoose.connect(
+            process.env.DATABASE_URL,
+            {dbName: process.env.DATABASE_NAME
+        }
+    );
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+        console.log(`Server running at http:localhost:${port}`);
+    });
+    } catch(error){
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+bootstrap();
 
  
--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -26,5 +26,4 @@ export class TasksRouter {
             res.json(newTask);
         })
     }
-
 }
